78185b73784133ba9324bc997ddbdfd9347fa757
Giulia-M <giuliamaggio16@gmail.com>
2021-05-05 01:48:51 +0200

    function toUserClick

--- myScript.js
+++ myScript.js
@@ -1,92 +1,103 @@
 const app = new Vue({
     el: "#app",
     data: {
-        
-      contacts: [
-        {
-          name: "Michele",
-          avatar: "img/avatar_1.jpg",
-          visible: true,
-          message: [
+
+        contacts: [
             {
-              date: "10/01/2020 15:30:55",
-              text: "Hai portato a spasso il cane?",
-              status: "sent",
+                name: "Michele",
+                avatar: "img/avatar_1.jpg",
+                visible: true,
+                message: [
+                    {
+                        date: "10/01/2020 15:30:55",
+                        text: "Hai portato a spasso il cane?",
+                        status: "sent",
+                    },
+                    {
+                        date: "10/01/2020 15:50:00",
+                        text: "Ricordati di dargli da mangiare",
+                        status: "sent",
+                    },
+                    {
+                        date: "10/01/2020 16:15:22",
+                        text: "Tutto fatto!",
+                        status: "received",
+                    },
+                ],
             },
             {
-              date: "10/01/2020 15:50:00",
-              text: "Ricordati di dargli da mangiare",
-              status: "sent",
+                name: "Fabio",
+                avatar: "img/avatar_2.jpg",
+                visible: true,
+                message: [
+                    {
+                        date: "20/03/2020 16:30:00",
+                        text: "Ciao come stai?",
+                        status: "sent",
+                    },
+                    {
+                        date: "20/03/2020 16:30:55",
+                        text: "Bene grazie! Stasera ci vediamo?",
+                        status: "received",
+                    },
+                    {
+                        date: "20/03/2020 16:35:00",
+                        text: "Mi piacerebbe ma devo andare a fare la spesa.",
+                        status: "sent",
+                    },
+                ],
             },
             {
-              date: "10/01/2020 16:15:22",
-              text: "Tutto fatto!",
-              status: "received",
-            },
-          ],
-        },
-        {
-          name: "Fabio",
-          avatar: "img/avatar_2.jpg",
-          visible: true,
-          message: [
-            {
-              date: "20/03/2020 16:30:00",
-              text: "Ciao come stai?",
-              status: "sent",
-            },
-            {
-              date: "20/03/2020 16:30:55",
-              text: "Bene grazie! Stasera ci vediamo?",
-              status: "received",
-            },
-            {
-              date: "20/03/2020 16:35:00",
-              text: "Mi piacerebbe ma devo andare a fare la spesa.",
-              status: "sent",
-            },
-          ],
-        },
-        {
-          name: "Samuele",
-          avatar: "img/avatar_3.jpg",
-          visible: true,
-          message: [
-            {
-              date: "28/03/2020 10:10:40",
-              text: "La Marianna va in campagna",
-              status: "received",
+                name: "Samuele",
+                avatar: "img/avatar_3.jpg",
+                visible: true,
+                message: [
+                    {
+                        date: "28/03/2020 10:10:40",
+                        text: "La Marianna va in campagna",
+                        status: "received",
+                    },
+                    {
+                        date: "28/03/2020 10:20:10",
+                        text: "Sicuro di non aver sbagliato chat?",
+                        status: "sent",
+                    },
+                    {
+                        date: "28/03/2020 16:15:22",
+                        text: "Ah scusa!",
+                        status: "received",
+                    },
+                ],
             },
             {
-              date: "28/03/2020 10:20:10",
-              text: "Sicuro di non aver sbagliato chat?",
-              status: "sent",
+                name: "Luisa",
+                avatar: "img/avatar_6.jpg",
+                visible: true,
+                message: [
+                    {
+                        date: "10/01/2020 15:30:55",
+                        text: "Lo sai che ha aperto una nuova pizzeria?",
+                        status: "sent",
+                    },
+                    {
+                        date: "10/01/2020 15:50:00",
+                        text: "Si, ma preferirei andare al cinema",
+                        status: "received",
+                    },
+                ],
             },
-            {
-              date: "28/03/2020 16:15:22",
-              text: "Ah scusa!",
-              status: "received",
-            },
-          ],
-        },
-        {
-          name: "Luisa",
-          avatar: "img/avatar_4.jpg",
-          visible: true,
-          message: [
-            {
-              date: "10/01/2020 15:30:55",
-              text: "Lo sai che ha aperto una nuova pizzeria?",
-              status: "sent",
-            },
-            {
-              date: "10/01/2020 15:50:00",
-              text: "Si, ma preferirei andare al cinema",
-              status: "received",
-            },
-          ],
-        },
-      ],
+        ],
+        activeUser: {},
     },
+    
+    
+    methods: {
+        toUserClick(user) {
+            this.activeUser = user
+        },
+        
+
+
+    }
 });
 
